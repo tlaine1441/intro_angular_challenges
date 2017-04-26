e8f0f05f05afe5f50de5e337dc361a0cb8a4bb3c
Taylor Laine <tlaine@msudenver.edu>
2017-04-26 01:05:05 -0600

    finishes challeng 3

--- challenge_3/js/app.js
+++ challenge_3/js/app.js
@@ -1,9 +1,25 @@
 console.log('challenge #3 -- app.js loaded!');
 
-// var app = angular.module("appThree", []);
-// app.controller("infoController", infoController)
+var app = angular.module("appThree", []);
+app.controller("infoController", infoController)
+app.controller("loanController", loanController)
 
-// function infoController(){
-//   this.name = "Jon Doe"; // default name
-//   this.height = 5.6; // default height in feet
-// }
+
+function infoController(){
+  this.name = "Jon Doe"; // default name
+  this.height = 5.6; // default height in feet
+}
+
+function loanController(){
+  this.amount = 500;
+  this.apr = 2.2;
+  this.month = (n) => {
+    var yearly_interest = (this.amount * (this.apr / 100) );
+    var monthly_interst = ( yearly_interest / 12 );
+    var accrued_interest = n * monthly_interst || 0;
+    return {
+      accrued_interest: accrued_interest,
+      total_balance: this.amount + accrued_interest
+    }
+  }
+}
